48cfac7a7dbe8a4d2fbe5c83c13be8f566e97338
Paku75 <paku2957@gmail.com>
2020-11-26 13:01:51 +0100

    front/db change

---
+++ ArpilabeCandidature1/Migrations/20201126115048_Change dbname.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArpilabeCandidature1.Migrations
+{
+    public partial class Changedbname : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
